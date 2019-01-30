fdfaf3f097511084b14ded0454f72e114cb231df
Seth Steichen <ssteiche@login3.cm.cluster>
2019-01-30 11:20:48 -0700

    testing

---
+++ assignments/02-bash-scripting-grad/gap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -u
+
+REGEX=${1:-*}
+DIR=$PWD/../../data/gapminder
+FILES_LIST=$(mktemp)
+find "$DIR" -type f -iregex ".*/$REGEX[^/]*$" | sort > "$FILES_LIST"
+
+
+NUM_LINES=$(wc -l "$FILES_LIST" | awk '{print $1}')
+
+if [[ $NUM_LINES -lt 1 ]]; then
+	    echo "There are no countries starting with \"$REGEX\""
+    fi
+
+i=0
+while read -r FILENAME; do 
+    BASENAME=$(basename "$FILENAME")
+    let i++
+    printf "%3d %s\n" $i "$BASENAME"
+done < "$FILES_LIST"
+
+rm "$FILES_LIST"
