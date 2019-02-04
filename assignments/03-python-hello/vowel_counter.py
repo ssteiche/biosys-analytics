370387a1fa59cc3d9a1c77cbb10272354e9d4b6c
Seth Steichen <ssteiche@login2.cm.cluster>
2019-02-04 11:54:45 -0700

    HW3 complete for real

--- assignments/03-python-hello/vowel_counter.py
+++ assignments/03-python-hello/vowel_counter.py
@@ -11,14 +11,23 @@ import sys
 
 # --------------------------------------------------
 def main():
-    STRING = sys.argv[1:]
+    string = sys.argv[1:]
 
-    if len(STRING) != 1:
+    if len(string) != 1:
         print('Usage: {} STRING'.format(os.path.basename(sys.argv[0])))
         sys.exit(1)
 
-    print('String is "{}"'.format(STRING))
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
-
+    count = 0
+    for letter in vowels:
+        count += str(string).count(letter)
+    
+    if count == 1:    
+       print('There is {} vowel in \"{}.\"'.format(count, string[0]))
+    elif count > 1:    
+       print('There are {} vowels in \"{}.\"'.format(count, string[0]))
+    elif count == 0:    
+       print('There are {} vowels in \"{}.\"'.format(count, string[0]))
 # --------------------------------------------------
 main()
